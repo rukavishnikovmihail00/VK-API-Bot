2720b43fa3fdb13cd67c6b1f4c1ce84ca74754ae
Mikhail Rukavishnikov <61332555+rukavishnikovmihail00@users.noreply.github.com>
2020-04-28 15:38:48 +0300

    Added weather report and timer setting

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboardButton, VkKeyboardColor, VkKeyboard
 import vk_api
 import random
-
-token = 'TOKEN'
+from bs4 import BeautifulSoup as BS
+import requests
+import time
+token = 'c3818740c6bf83c97ff76b2988aab95ed06be60326a8db18bc979b908b0c50db76a02283834dff0c4bfc8'
 
 session = vk_api.VkApi(token = token)
 vk = session.get_api()
@@ -18,21 +20,46 @@ def random_id():
     return Random
 
 def createKeyboard(response):
-    keyboard = VkKeyboard(one_time=True)
-    if response == 'начать':
-        keyboard.add_button('Найти лица на фотографии', color = VkKeyboardColor.POSITIVE)
+    keyboard = VkKeyboard(one_time=False)
+    if response == 'начать' or response == 'меню':
+        keyboard.add_button('Узнать погоду', color = VkKeyboardColor.POSITIVE)
+        keyboard.add_line()
+        keyboard.add_button('Установить таймер', color = VkKeyboardColor.PRIMARY)
 
     keyboard = keyboard.get_keyboard()
     return keyboard
 
+
+def get_weather():
+    r = requests.get('https://sinoptik.ua/погода-нижний-новгород')
+    html = BS(r.content, 'html.parser')
+
+    for el in html.select('#content'):
+        t_min = el.select('.temperature .min')[0].text
+        t_max = el.select('.temperature .max')[0].text
+        text = el.select('.wDescription .description')[0].text
+    session.method('messages.send', {'user_id': event.user_id, 'message': text + '\n' + t_min + '\n' + t_max, 'random_id': random_id()})
+
+
+def set_timer(response):
+    time.sleep(int(response))
+    session.method('messages.send',  {'user_id': event.user_id, 'message': 'Таймер закончился', 'random_id': random_id()})
+
+
 while True:
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
+        if event.type == VkEventType.MESSAGE_NEW and event.from_user:
             response = event.text.lower()
             if event.from_user:
-                if response == 'начать':
+
+                if response == 'начать' or response == 'меню':
                     keyboard = createKeyboard(response)
                     session.method('messages.send', {'user_id': event.user_id, 'message': 'Что ты хочешь сделать?', 'random_id': random_id(), 'keyboard': keyboard})
-            elif event.from_chat:
-                if response == 'Найти лица на фотографии':
-                    session.method('messages.send', {'user_id': event.user_id, 'message': 'Сработало', 'random_id': random_id()})
+                elif response == 'узнать погоду':
+                    get_weather()
+                elif response == 'установить таймер':
+                    session.method('messages.send', {'user_id': event.user_id, 'message': 'Введите время в секундах: ', 'random_id': random_id()})
+                elif response.isdigit():
+                    set_timer(response)
+
+
